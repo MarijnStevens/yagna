f5a04dff893e0cacaad57a5990f4bbf649989e96
Marijn Stevens <marijnstevens@gmail.com>
2021-05-07 14:28:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,27 +4,25 @@
 # 
 # packages:  libssl-dev:aarch64 
 # sudo apt-get install libglib2.0
-# ARCH="aarch64"
+ARCH="aarch64"
+TARGET="$ARCH-unknown-linux-gnu"
 
-ARCH="armv7"
-TARGET="$ARCH-unknown-linux-gnueabihf"
+#ARCH="armv7"
+#TARGET="$ARCH-unknown-linux-gnueabihf"
+
 TARGET_DIR="./target/$TARGET/release/"
 
 PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
+ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- \
 cargo build --release --target=$TARGET -p yagna -p ya-exe-unit -p ya-provider -p golemsp -p gftp
-
-
-echo -e Export to $TARGET_DIR
-
 echo -e $(readelf --arch-specific "$TARGET_DIR/yagna") && \
-
-echo -e Export to $TARGET_DIR
-\cp "$TARGET_DIR/exe-unit" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/gftp" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/yagna" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/golemsp" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/libgftp.rlib" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/libya_exe_unit.rlib" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/libgftp.rlib" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/libya_provider.rlib" "../yagna-binaries/$ARCH/"
-\cp "$TARGET_DIR/ya-provider" "../yagna-binaries/$ARCH/"
+mkdir -p ../yagna-binaries/$ARCH/ && \
+\cp "$TARGET_DIR/exe-unit" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/gftp" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/yagna" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/golemsp" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/libgftp.rlib" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/libya_exe_unit.rlib" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/libgftp.rlib" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/libya_provider.rlib" "../yagna-binaries/$ARCH/" && \
+\cp "$TARGET_DIR/ya-provider" "../yagna-binaries/$ARCH/" && \
