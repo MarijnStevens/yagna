0cf1b6119937803d9694f1101c8ab49e59a074f7
Marijn Stevens <marijnstevens@gmail.com>
2021-06-06 23:26:29 +0200

    Cleanup. Removed rlibs.

--- build.sh
+++ build.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-
-# 
-# packages:  libssl-dev:aarch64 
-# sudo apt-get install libglib2.0
 ARCH="aarch64"
 TARGET="$ARCH-unknown-linux-gnu"
-
-#ARCH="armv7"
-#TARGET="$ARCH-unknown-linux-gnueabihf"
-
 TARGET_DIR="./target/$TARGET/release"
 
 PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
 cargo build --release --target=$TARGET -p yagna -p ya-exe-unit -p ya-provider -p golemsp -p gftp
-echo -e $(readelf --arch-specific "$TARGET_DIR/yagna") && \
+
 mkdir -p ../yagna-binaries/ && \
-ls 
-
-#exit 0
-
 \cp "$TARGET_DIR/exe-unit" "../yagna-binaries/" && \
 \cp "$TARGET_DIR/gftp" "../yagna-binaries/" && \
 \cp "$TARGET_DIR/yagna" "../yagna-binaries/" && \
 \cp "$TARGET_DIR/golemsp" "../yagna-binaries/" && \
-\cp "$TARGET_DIR/libgftp.rlib" "../yagna-binaries/" && \
-\cp "$TARGET_DIR/libya_exe_unit.rlib" "../yagna-binaries/" && \
-\cp "$TARGET_DIR/libgftp.rlib" "../yagna-binaries/" && \
-\cp "$TARGET_DIR/libya_provider.rlib" "../yagna-binaries/" && \
 \cp "$TARGET_DIR/ya-provider" "../yagna-binaries/" 
