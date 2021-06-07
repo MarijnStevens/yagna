fd0b9d8aa5503d6d5fd0345905713804279daa88
Marijn Stevens <marijnstevens@gmail.com>
2021-06-07 02:54:03 +0200

    Set stable toolchain.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ TARGET="$ARCH-unknown-linux-gnu"
 TARGET_DIR="./target/$TARGET/release"
 
 PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_STATIC=true \
-cargo build --release --target=$TARGET -p yagna -p ya-exe-unit -p ya-provider -p golemsp -p gftp
+RUSTUP_TOOLCHAIN=stable cargo build --release --target=$TARGET -p yagna -p ya-exe-unit -p ya-provider -p golemsp -p gftp
 
 mkdir -p ../yagna-binaries/ && \
 \cp "$TARGET_DIR/exe-unit" "../yagna-binaries/" && \
